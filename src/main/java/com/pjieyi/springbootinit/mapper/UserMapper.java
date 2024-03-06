3af2150a15dc4d1270af211060be76bb629e8170
jieyi123 <pjieyi@foxmail.com>
2024-03-06 16:31:48 +0800

    initial commit

---
+++ src/main/java/com/pjieyi/springbootinit/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.pjieyi.springbootinit.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pjieyi.springbootinit.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
